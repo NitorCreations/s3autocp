0157a76f3653d761362ef67164c7a2ece4b248da
Jouni Tenhunen <jouni.tenhunen@nitor.com>
2023-10-26 13:41:24 +0300

    Add brotli and gzip compression

--- src/s3autocp/s3autocp.py
+++ src/s3autocp/s3autocp.py
@@ -1,7 +1,11 @@
 import argparse
+import brotli
+import gzip
 import json
+import multiprocessing
 import os
 import re
+from collections.abc import Callable
 from glob import iglob
 from typing import Iterator, Tuple
 
@@ -13,93 +17,102 @@ HASH_IN_FILENAME_REGEX = re.compile(r".*[\.\-][0-9a-fA-F]+[\..*]+")
 
 s3_client = boto3.client("s3")
 
+MIME_TYPES = {
+    "aac": "audio/aac",
+    "abw": "application/x-abiword",
+    "arc": "application/x-freearc",
+    "avif": "image/avif",
+    "avi": "video/x-msvideo",
+    "azw": "application/vnd.amazon.ebook",
+    "bin": "application/octet-stream",
+    "bmp": "image/bmp",
+    "br": "application/x-br",
+    "bz": "application/x-bzip",
+    "bz2": "application/x-bzip2",
+    "cda": "application/x-cdf",
+    "csh": "application/x-csh",
+    "css": "text/css",
+    "csv": "text/csv",
+    "doc": "application/msword",
+    "docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "eot": "application/vnd.ms-fontobject",
+    "epub": "application/epub+zip",
+    "gz": "application/gzip",
+    "gif": "image/gif",
+    "htm": "text/html",
+    "html": "text/html",
+    "ico": "image/vnd.microsoft.icon",
+    "ics": "ext/calendar",
+    "jar": "application/java-archive",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "js": "text/javascript",
+    "json": "application/json",
+    "jsonld": "application/ld+json",
+    "map": "application/json",
+    "md": "text/markdown",
+    "mid": "audio/midi",
+    "midi": "audio/midi",
+    "mjs": "text/javascript",
+    "mp3": "audio/mpeg",
+    "mp4": "video/mp4",
+    "mpeg": "video/mpeg",
+    "mpkg": "application/vnd.apple.installer+xml",
+    "odp": "application/vnd.oasis.opendocument.presentation",
+    "ods": "application/vnd.oasis.opendocument.spreadsheet",
+    "odt": "application/vnd.oasis.opendocument.text",
+    "oga": "audio/ogg",
+    "ogv": "video/ogg",
+    "ogx": "application/ogg",
+    "opus": "audio/opus",
+    "otf": "font/otf",
+    "png": "image/png",
+    "pdf": "application/pdf",
+    "php": "application/x-httpd-php",
+    "ppt": "application/vnd.ms-powerpoint",
+    "pptx": "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    "rar": "application/vnd.rar",
+    "rtf": "application/rtf",
+    "sh": "application/x-sh",
+    "svg": "image/svg+xml",
+    "tar": "application/x-tar",
+    "tif": "image/tiff",
+    "tiff": "image/tiff",
+    "ts": "video/mp2t",
+    "ttf": "font/ttf",
+    "txt": "text/plain",
+    "vsd": "application/vnd.visio",
+    "wav": "audio/wav",
+    "weba": "audio/webm",
+    "webm": "video/webm",
+    "webp": "image/webp",
+    "woff": "font/woff",
+    "woff2": "font/woff2",
+    "xhtml": "application/xhtml+xml",
+    "xls": "application/vnd.ms-excel",
+    "xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "xml": "application/xml",
+    "xul": "application/vnd.mozilla.xul+xml",
+    "yaml": "application/yaml",
+    "yml": "application/yaml",
+    "zip": "application/zip",
+    "3gp": "video/3gpp",
+    "3g2": "video/3gpp2",
+    "7z": "application/x-7z-compressed",
+}
+
 
 def _get_mime_type(filename: str) -> str:
-    mime_types = {
-        "aac": "audio/aac",
-        "abw": "application/x-abiword",
-        "arc": "application/x-freearc",
-        "avif": "image/avif",
-        "avi": "video/x-msvideo",
-        "azw": "application/vnd.amazon.ebook",
-        "bin": "application/octet-stream",
-        "bmp": "image/bmp",
-        "bz": "application/x-bzip",
-        "bz2": "application/x-bzip2",
-        "cda": "application/x-cdf",
-        "csh": "application/x-csh",
-        "css": "text/css",
-        "csv": "text/csv",
-        "doc": "application/msword",
-        "docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        "eot": "application/vnd.ms-fontobject",
-        "epub": "application/epub+zip",
-        "gz": "application/gzip",
-        "gif": "image/gif",
-        "htm": "text/html",
-        "html": "text/html",
-        "ico": "image/vnd.microsoft.icon",
-        "ics": "ext/calendar",
-        "jar": "application/java-archive",
-        "jpeg": "image/jpeg",
-        "jpg": "image/jpeg",
-        "js": "text/javascript",
-        "json": "application/json",
-        "jsonld": "application/ld+json",
-        "map": "application/json",
-        "mid": "audio/midi",
-        "midi": "audio/midi",
-        "mjs": "text/javascript",
-        "mp3": "audio/mpeg",
-        "mp4": "video/mp4",
-        "mpeg": "video/mpeg",
-        "mpkg": "application/vnd.apple.installer+xml",
-        "odp": "application/vnd.oasis.opendocument.presentation",
-        "ods": "application/vnd.oasis.opendocument.spreadsheet",
-        "odt": "application/vnd.oasis.opendocument.text",
-        "oga": "audio/ogg",
-        "ogv": "video/ogg",
-        "ogx": "application/ogg",
-        "opus": "audio/opus",
-        "otf": "font/otf",
-        "png": "image/png",
-        "pdf": "application/pdf",
-        "php": "application/x-httpd-php",
-        "ppt": "application/vnd.ms-powerpoint",
-        "pptx": "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-        "rar": "application/vnd.rar",
-        "rtf": "application/rtf",
-        "sh": "application/x-sh",
-        "svg": "image/svg+xml",
-        "tar": "application/x-tar",
-        "tif": "image/tiff",
-        "tiff": "image/tiff",
-        "ts": "video/mp2t",
-        "ttf": "font/ttf",
-        "txt": "text/plain",
-        "vsd": "application/vnd.visio",
-        "wav": "audio/wav",
-        "weba": "audio/webm",
-        "webm": "video/webm",
-        "webp": "image/webp",
-        "woff": "font/woff",
-        "woff2": "font/woff2",
-        "xhtml": "application/xhtml+xml",
-        "xls": "application/vnd.ms-excel",
-        "xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        "xml": "application/xml",
-        "xul": "application/vnd.mozilla.xul+xml",
-        "zip": "application/zip",
-        "3gp": "video/3gpp",
-        "3g2": "video/3gpp2",
-        "7z": "application/x-7z-compressed",
-    }
     parts = filename.split(".")
     extension = parts[-1]
-    if extension in ("gz", "br") and parts[-2] in mime_types:
+    if extension in ("gz", "br") and parts[-2] in MIME_TYPES:
         extension = parts[-2]
-    return mime_types.get(extension, DEFAULT_CONTENT_TYPE)
+    return MIME_TYPES.get(extension, DEFAULT_CONTENT_TYPE)
 
+def _should_compress(filename: str) -> bool:
+    non_text_compressibles = ["json", "map", "svg", "ico", "yaml", "yml", "xml"]
+    compressibles = [key for key, value in MIME_TYPES.items() if value.startswith("text")] + non_text_compressibles
+    return filename.split(".")[-1] in compressibles
 
 def _get_args() -> Tuple[str, str]:
     parser = argparse.ArgumentParser(
@@ -168,6 +181,28 @@ def _copy(filename: str, bucket: str, key: str) -> None:
         )
 
 
+def _compress_file(filename: str) -> list[str]:
+    if _should_compress(filename):
+        with open(filename, 'rb') as f_input:
+            data = f_input.read()
+        brotli_data = brotli.compress(data, quality=11)
+        gzip_data = gzip.compress(data, compresslevel=9)
+        with open(f"{filename}.br", 'wb') as f_output:
+            f_output.write(brotli_data)
+        with open(f"{filename}.gz", 'wb') as f_output:
+            f_output.write(gzip_data)
+        return filename
+    else:
+        return None
+
+def _upload(filename:str, bucket: str, path: str, source_dir: str) -> None:
+    key = f'{path}{filename.replace(source_dir, "").replace("//", "/")}'
+    if key.startswith("/"):
+        key = key[1:]
+    _copy(filename=filename, bucket=bucket, key=key)
+    print(f"upload: {filename} s3://{bucket}/{key}")
+
+
 def s3autocp():
     source, destination = _get_args()
     bucket_name, path = _get_bucket_name_and_path(
@@ -179,12 +214,18 @@ def s3autocp():
         _get_filenames(source_dir=source),
         key=lambda filename: "index.htm" in filename,
     )
-    for filename in filenames:
-        key = f'{path}{filename.replace(source, "").replace("//", "/")}'
-        if key.startswith("/"):
-            key = key[1:]
-        _copy(filename=filename, bucket=bucket_name, key=key)
-        print(f"upload: {filename} s3://{bucket_name}/{key}")
+    print("compressing files...", end="")
+    pool = multiprocessing.Pool()
+    compressed_files = pool.map(_compress_file, filenames)
+    pool.close()
+    pool.join()
+    print("done!")
+    for filename in compressed_files:
+        if filename:
+            filenames += [f"{filename}.br", f"{filename}.gz"]
+    filenames = set(filenames)            
+    for filename in sorted(filenames):
+        _upload(filename=filename, bucket=bucket_name, path=path, source_dir=source)
 
 
 if __name__ == "__main__":
